94e57e1d550676acfcecb4d1b0091479fedc40ea
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-04-06 09:37:26 +0530

    modifier

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ package Eis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class Test {
 
 	public static void main(String[] args) {
